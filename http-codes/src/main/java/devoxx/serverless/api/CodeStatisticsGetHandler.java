4f34002d4eb9eac041af145c0e36630912dd50da
Frederic Boulet <frederic.boulet@gmail.com>
2017-03-22 15:11:36 +0100

    c

---
+++ http-codes/src/main/java/devoxx/serverless/api/CodeStatisticsGetHandler.java
@@ -0,0 +1,28 @@
+package devoxx.serverless.api;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import copy.of.com.amazonaws.serverless.proxy.internal.model.AwsProxyRequest;
+import copy.of.com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse;
+
+public class CodeStatisticsGetHandler extends AbstractGetHandler {
+
+    private final DynamoDB dynamoDB;
+
+    public CodeStatisticsGetHandler() {
+        this.dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+
+    }
+
+    @Override
+    public AwsProxyResponse doHandle(AwsProxyRequest request) throws Exception {
+
+        Table table = dynamoDB.getTable("HttpCodeStatistics");
+
+        Item item = table.getItem("Code", request.getPathParameters().get("code"));
+
+        return createSuccessResponse(response);
+    }
+}
